dd540d533f7634d26e9a6d461c2f05f35dedfcca
Michael Smoor <michael_smoor@hotmail.com>
2015-06-25 14:45:30 +0200

    Added macro calculator

--- app/src/main/java/smoor/michael/smartconsumption/MainActivity.java
+++ app/src/main/java/smoor/michael/smartconsumption/MainActivity.java
@@ -7,8 +7,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
 
@@ -16,9 +20,11 @@ import android.widget.TextView;
 public class MainActivity extends ActionBarActivity {
 
     ImageButton ibutEen, ibutTwee;
-    Button btnInhoud;
-    TextView loginTxt;
+    Button btnInhoud, btnMacro;
+    TextView loginTxt, txtCalorieen;
+    EditText textGeslacht, textGewicht, textLeeftijd, textLengte;
     WebView webData;
+    Spinner dropDownActief;
     static TabHost tabHost;
     private Inhoud dbInhoud = null;
 
@@ -30,10 +36,17 @@ public class MainActivity extends ActionBarActivity {
         ibutEen = (ImageButton) findViewById(R.id.IBEen);
         ibutTwee = (ImageButton) findViewById(R.id.IBTwee);
         btnInhoud = (Button) findViewById(R.id.btnData);
+        btnMacro = (Button) findViewById(R.id.btnBereken);
         loginTxt = (TextView) findViewById(R.id.txtLogin);
+        txtCalorieen = (TextView) findViewById(R.id.txtCalorie);
+        textGeslacht = (EditText) findViewById(R.id.txtGeslacht);
+        textGewicht = (EditText) findViewById(R.id.txtGewicht);
+        textLeeftijd = (EditText) findViewById(R.id.txtLeeftijd);
+        textLengte = (EditText) findViewById(R.id.txtLengte);
         tabHost = (TabHost) findViewById(R.id.tabHost);
         webData = (WebView) findViewById(R.id.webViewData);
         webData.setWebViewClient(new WebViewClient());
+        dropDownActief = (Spinner) findViewById(R.id.ddActief);
 
 
         final TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
@@ -62,6 +75,13 @@ public class MainActivity extends ActionBarActivity {
 
         dbInhoud = new Inhoud(this);
 
+        String[] actief={"Niet actief", "Licht actief", "Gemiddeld actief", "Erg actief", "Extreem actief"};
+        ArrayAdapter<String> stringArrayAdapter=
+                new ArrayAdapter<String>(
+                        this,
+                        android.R.layout.simple_spinner_dropdown_item,
+                        actief);
+        dropDownActief.setAdapter(stringArrayAdapter);
     }
 
     @Override
@@ -97,5 +117,40 @@ public class MainActivity extends ActionBarActivity {
     public void ButtonData(View v) {
         tabHost.setCurrentTab(2);
         webData.loadUrl("http://tweakers.net/");
+    }
+
+    public void ButtonMacro (View v) {
+        String g = textGewicht.getText().toString();
+        double gewicht = Double.parseDouble(g);
+        String l = textLengte.getText().toString();
+        double lengte = Double.parseDouble(l);
+        String le = textLeeftijd.getText().toString();
+        double leeftijd = Double.parseDouble(le);
+        Double bmr;
+        String geslacht = textGeslacht.getText().toString();
+        String actief = dropDownActief.getSelectedItem().toString();
+
+        if (geslacht == "Man") {
+            bmr = ((9.99 * gewicht) + (6.25 * lengte) - (4.92 * leeftijd) + 5);
+        }
+        else {
+            bmr = ((9.99 * gewicht) + (6.25 * lengte) - (4.92 * leeftijd) - 161);
+        }
+
+        if (actief == "Niet actief") {
+            bmr *= 1.2;
+        }
+        else if (actief == "Licht actief") {
+            bmr *= 1.35;
+        }
+        else if (actief == "Gemiddeld actief") {
+            bmr *= 1.55;
+        }
+        else if (actief == "Erg actief") {
+            bmr *= 1.77;
+        }
+        else {
+            bmr *= 2.05;
+        }
     }
 }
